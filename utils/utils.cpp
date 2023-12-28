1e7a7695cc7255a3c8450eed4d4a0355a4f933be
bristinemuluh <bristinemuluh@gmail.com>
2023-12-28 13:40:33 -0600

    Created a util (find_closing) for isolation text between matching delimeters

--- utils/utils.cpp
+++ utils/utils.cpp
@@ -35,3 +35,29 @@ void remove_intances_of_char(string &str, char c)
     string::iterator endPos = remove(str.begin(), str.end(), c);
     str.erase(endPos, str.end());
 }
+
+/**
+ * Get the index of the closing char.
+ * Example: given (ab ( cd ) ef (gh)) with opening_idx 4, and delims ( and )
+ *          returns 9
+ * NOTE: returns opening_idx for wrongly formatted string
+*/
+size_t find_closing(string& str, size_t opening_idx, char opening_delim, char closing_delim)
+{
+    size_t opened = 1;
+    size_t current_idx = opening_idx + 1;
+
+    while (opened !=0 && current_idx < str.length())
+    {
+        if(str.at(current_idx) == opening_delim) opened++;
+        else if(str.at(current_idx) == closing_delim) opened--;
+
+        if(opened > str.length()) return opening_idx;
+
+        current_idx++;
+    }
+    
+    if(opened == 0) return current_idx -1;
+    else return opening_idx;
+}
+
--- utils/utils.hpp
+++ utils/utils.hpp
@@ -11,3 +11,5 @@ vector<string> split_str(string &str, char delim);
 vector<string> split_str(string &str, string delim);
 
 void remove_intances_of_char(string &str, char c);
+
+size_t find_closing(string& str, size_t opening_idx, char opening_delim, char closing_delim);
